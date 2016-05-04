744d43da9bd3473947b2b2a58fe6a9af7e9a1dea
Rodrigo Soares <silvars1@gmail.com>
2016-05-04 18:18:48 -0300

    Added abstract column to Posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425151757) do
+ActiveRecord::Schema.define(version: 20160504204248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160425151757) do
     t.string  "content"
     t.boolean "published"
     t.string  "slug"
+    t.string  "abstract"
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
