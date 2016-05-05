65a4f156071e86d353cc0e3893f61dfa838af4f7
Rodrigo Soares <silvars1@gmail.com>
2016-05-05 12:34:56 -0300

    Fixed page titles.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,7 +5,7 @@ module PagesHelper
       if page_title.empty?
           base_title
       else
-          "#{base_title} - #{page_title}"
+          "#{page_title} - #{base_title}"
       end
   end
 end
