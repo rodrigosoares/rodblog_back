d95f774766e9073c3033d51f43056acd7f3eb16a
Rodrigo Soares <silvars1@gmail.com>
2016-05-06 17:03:48 -0300

    Created basic index page for Projects.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -16,6 +16,8 @@ module PagesHelper
       { title: 'Sobre Mim', subheading: 'Um pouco da minha trajetória acadêmica e profissional.', image_url: '/assets/clean_blog/about-bg.jpg' }
     elsif current_page? contact_path
       { title: 'Contato', subheading: 'Alguma dúvida ou contato profissional? Me mande uma mensagem!', image_url: '/assets/clean_blog/contact-bg.jpg' }
+    elsif current_page? projects_path
+      { title: 'Portfólio', subheading: 'Projetos Web que atuei no desenvolvimento.', image_url: '/assets/clean_blog/contact-bg.jpg' }
     end
   end
 end
