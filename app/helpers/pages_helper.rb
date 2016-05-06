df5fd3e60fd2a066e88582b07d8b50986f9240ad
Rodrigo Soares <silvars1@gmail.com>
2016-05-06 11:16:14 -0300

    Moved header logic to helper.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -8,4 +8,14 @@ module PagesHelper
           "#{page_title} - #{base_title}"
       end
   end
+
+  def header_info
+    if current_page? root_path
+      { title: 'RodBlog', subheading: 'O blog do Rodrigo sobre a vida acadêmica.', image_url: '/assets/clean_blog/home-bg.jpg' }
+    elsif current_page? about_path
+      { title: 'Sobre Mim', subheading: 'Um pouco sobre a minha trajetória acadêmica e profissional.', image_url: '/assets/clean_blog/about-bg.jpg' }
+    elsif current_page? contact_path
+      { title: 'Contato', subheading: 'Alguma dúvida ou contato profissional? Me mande uma mensagem!', image_url: '/assets/clean_blog/contact-bg.jpg' }
+    end
+  end
 end
