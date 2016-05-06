9f84f2e91994801d90e36f7fa2119ecb0226c0ca
Rodrigo Soares <silvars1@gmail.com>
2016-05-06 13:54:30 -0300

    Changed some front-end texts.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -11,9 +11,9 @@ module PagesHelper
 
   def header_info
     if current_page? root_path
-      { title: 'RodBlog', subheading: 'O blog do Rodrigo sobre a vida acadêmica.', image_url: '/assets/clean_blog/home-bg.jpg' }
+      { title: 'Rodrigo Soares', subheading: 'Desenvolvedor Ruby on Rails.', image_url: '/assets/clean_blog/home-bg.jpg' }
     elsif current_page? about_path
-      { title: 'Sobre Mim', subheading: 'Um pouco sobre a minha trajetória acadêmica e profissional.', image_url: '/assets/clean_blog/about-bg.jpg' }
+      { title: 'Sobre Mim', subheading: 'Um pouco da minha trajetória acadêmica e profissional.', image_url: '/assets/clean_blog/about-bg.jpg' }
     elsif current_page? contact_path
       { title: 'Contato', subheading: 'Alguma dúvida ou contato profissional? Me mande uma mensagem!', image_url: '/assets/clean_blog/contact-bg.jpg' }
     end
